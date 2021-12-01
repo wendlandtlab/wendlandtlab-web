2c31dbd5e6ed878cce996c07670a0f65a2eae8eb
Gino Occhialini <gino.occhialini@gmail.com>
2021-11-30 22:49:22 -0500

    header less is more

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -24,33 +24,6 @@
           <router-link to="/publications" class="text-base font-medium text-gray-500 hover:text-gray-900">
             Publications
           </router-link>
-
-          <Popover class="relative" v-slot="{ open }">
-            <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500']">
-              <span>More</span>
-              <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
-            </PopoverButton>
-
-            <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-              <PopoverPanel class="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
-                <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                    <router-link v-for="item in resources" :key="item.name" :to="item.href" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                      <component :is="item.icon" class="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                      <div class="ml-4">
-                        <p class="text-base font-medium text-gray-900">
-                          {{ item.name }}
-                        </p>
-                        <p class="mt-1 text-sm text-gray-500">
-                          {{ item.description }}
-                        </p>
-                      </div>
-                    </router-link>
-                  </div>
-                </div>
-              </PopoverPanel>
-            </transition>
-          </Popover>
         </PopoverGroup>
       </div>
     </div>
@@ -82,9 +55,6 @@
               <router-link to="/publications" class="text-base font-medium text-gray-900 hover:text-gray-700">
                 Publications
               </router-link>
-              <router-link v-for="item in resources" :key="item.name" :to="item.href" class="text-base font-medium text-gray-900 hover:text-gray-700">
-                {{ item.name }}
-              </router-link>
             </div>
           </div>
         </div>
@@ -111,27 +81,6 @@ import {
 } from '@heroicons/vue/outline'
 import { ChevronDownIcon } from '@heroicons/vue/solid'
 
-const resources = [
-  {
-    name: 'What we stand for',
-    description: 'Group values',
-    href: '#',
-    icon: SupportIcon,
-  },
-  {
-    name: 'Lab space and resources',
-    description: 'A tour of our lab',
-    href: '#',
-    icon: BookmarkAltIcon,
-  },
-  {
-    name: 'Fun and events',
-    description: 'See what we do outside of chemistry',
-    href: '#',
-    icon: CalendarIcon,
-  }
-]
-
 export default {
   components: {
     Popover,
@@ -144,7 +93,6 @@ export default {
   },
   setup() {
     return {
-      resources,
     }
   },
 }
