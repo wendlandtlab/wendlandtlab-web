63c67eb5b128b66688e9fd672126f7b29e418c89
Gino Occhialini <gino.occhialini@gmail.com>
2021-12-02 22:31:36 -0500

    New framework for research page

--- src/pages/Research.vue
+++ src/pages/Research.vue
@@ -10,32 +10,60 @@ export default {
   <div class="flex flex-col min-h-screen">
     <HeaderMenu />
     <div class="flex-grow">
-      <div class="container mx-auto max-w-screen-xl">
+      <div class="container mx-auto px-5 max-w-screen-xl">
+        <div class="flex flex-col mx-auto sm:flex-row align-middle px-5">
+          <div
+            class="
+              w-18
+              h-18
+              rounded-full
+              flex
+              px-5
+              mx-5
+              align-middle
+              mx-auto
+              justify-center
+              items-center
+              text-red-700
+              bg-red-200
+            "
+          >
+            <Icon
+              icon="grommet-icons:cycle"
+              width="35"
+              class="iconify-inline"
+            />
+          </div>
+          <div class="text-justify flex-grow text-md text-gray-600 pb-3">
+            <p>
+              We are excited about developing new
+              <b class="text-red-700 text-lg">catalytic methods</b> for
+              selective organic transformations. Although we are excited to
+              explore any area our chemistry and imagination takes us, lately
+              our interest has been captured by new redox-neutral
+              <i>editing</i> approaches to organic synthesis&#8212;particularly
+              those utilizing mild and selective radical chemistry. In
+              identifying important chemical problems, we position ourselves to
+              unravel new <b class="text-green-600"> mechanisms</b> using a
+              strong background in physical organic techniques, framed by
+              meaninful <b class="text-blue-700">applications</b>, such as in
+              the synthesis of bio-relevant molecules. To illustrate this, we
+              have outlined our research areas from each of these two lenses.
+            </p>
+          </div>
+        </div>
         <div class="flex flex-wrap justify-evenly pt-5 px-4 my-5">
-          <ResearchArea
-            Area="New Catalysis"
-            Icon="grommet-icons:cycle"
-            IconColor="red"
-            Description="Design and discovery of new catalytic methods to expand chemical understanding and synthetic potential"
-          />
           <ResearchArea
             Area="New Mechanisms"
             Icon="eos-icons:rotating-gear"
             IconColor="green"
-            Description="Elucidation of new mechanistic principles to expand or compliment powerful existing reaction maifolds"
+            Description="Elucidation of new mechanistic principles to expand or compliment powerful existing reaction maifolds  <<READ MORE>>"
           />
           <ResearchArea
             Area="New Applications"
             Icon="fluent:toolbox-12-filled"
             IconColor="blue"
-            Description="Identification and refinement of important methodologies for selective catalytic processes"
-          />
-        </div>
-        <div class="">
-          <img
-            src="https://wendlandtlab.com/wp-content/uploads/2020/10/Image-from-iOS-1-scaled.jpg"
-            alt=""
-            class="object-cover overflow-hidden rounded-md h-100 w-full"
+            Description="Identification and refinement of important methodologies for selective catalytic processes  <<READ MORE>>"
           />
         </div>
         <div class="text-2xl font-sans font-bold my-5">Who funds us:</div>
@@ -44,27 +72,27 @@ export default {
             <img
               src="../assets/mit-logo-notext.svg?url"
               alt="MIT"
-              class="object-contain overflow-hidden  h-30"
+              class="object-contain overflow-hidden py-4 h-30"
             />
           </a>
           <a href="https://www.nih.gov/">
             <img
               src="../assets/nih-logo.svg?url"
               alt="NIH"
-              class="object-contain overflow-hidden h-30"
+              class="object-contain overflow-hidden py-4 h-30"
           /></a>
           <a
             href="https://www.acs.org/content/acs/en/funding/grants/petroleum-research-fund.html"
             ><img
               src="../assets/prf-logo.jpg"
               alt="ACS PRF"
-              class="object-contain overflow-hidden-md h-30"
+              class="object-contain overflow-hidden-md py-4 h-30"
           /></a>
           <a href="https://www.beckman-foundation.org/"
             ><img
               src="../assets/beckman-logo.png"
               alt="Beckman Foundation"
-              class="object-contain overflow-hidden h-30"
+              class="object-contain overflow-hidden py-4 h-30"
           /></a>
         </div>
       </div>
