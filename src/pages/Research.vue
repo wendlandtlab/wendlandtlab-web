a1e4478038230989115d56b1458401e56b31c29d
Gino Occhialini <gino.occhialini@gmail.com>
2022-03-10 01:03:50 -0500

    Research page add sugar/editing

--- src/pages/Research.vue
+++ src/pages/Research.vue
@@ -70,12 +70,103 @@ export default {
           <img
             src="../assets/research/therm-isom.svg?url"
             alt="thermodynamic isomerization energy diagram"
-            class="object-contain overflow-hidden py-2 h-sm"
+            class="object-contain overflow-hidden py-2 w-120"
           />
           <img
             src="../assets/research/kinetic-isom.svg?url"
             alt="kinetic isomerization energy diagram"
-            class="object-contain overflow-hidden py-2 h-sm"
+            class="object-contain overflow-hidden py-2 w-120"
+          />
+        </div>
+        <div class="text-xl text-gray-600 font-sans font-bold my-1">
+          Stereochemical editing:
+        </div>
+        <div class="text-justify text-md text-gray-600">
+          <p class="mb-2">
+            Stereochemistry forms the foundation of the structure-function
+            relationship in natural products and biomolecules. The selective
+            installation of stereocenters remains one of the chemist's greatest
+            tools and challenges. In traditional synthetic strategies, the
+            desired stereochemistry is conferred during the core synthesis, and
+            often synthetic approahces are built around desired stereochemical
+            outcomes. Late-stage stereochemical
+            <a
+              href="https://wendlandtlab.blob.core.windows.net/pubs/2021-JACS-ZhangGu-article.pdf"
+              class="text-blue-700 font-bold"
+              >editing</a
+            >
+            delivers the ability to diversify this approach. Imagine a molecule
+            as a jigsaw puzzle, with the intricate spatial arangement of atoms
+            in space as the pieces. To change one piece, would you rather take
+            apart the puzzle or carefully excise just the piece of interest?
+          </p>
+        </div>
+        <div class="flex flex-wrap justify-evenly px-4 pb-5">
+          <img
+            src="../assets/research/streptomycin.svg?url"
+            alt="streptomycin structure"
+            class="object-contain overflow-hidden rounded-md py-2 w-64"
+          />
+          <img
+            src="../assets/research/puzzle.svg?url"
+            alt="puzzle analogy"
+            class="object-contain overflow-hidden py-2 w-sm"
+          />
+          <img
+            src="../assets/research/puzzle-part.svg?url"
+            alt="puzzle visual of molecule"
+            class="object-contain overflow-hidden rounded-md py-2 w-sm"
+          />
+        </div>
+        <div class="text-xl text-gray-600 font-sans font-bold my-1">
+          Selective synthesis of "rare" monosaccharides:
+        </div>
+        <div class="text-justify text-md text-gray-600">
+          <p class="mb-2">
+            Carbohydrates are a key component of life, implicated in essential
+            biological processes - cell signaling, mechanical structure, and
+            energy storage. Hundreds of distinct monosaccharides, known as “rare
+            sugars”, feature prominently in bioactive natural products and are
+            under investigation as signaling agents in biological pathways,
+            active chemophores in pharmaceuticals, and as low-glycemic
+            sweeteners. However, despite their importance to human health, the
+            exploration of rare and unnatural sugars as biologically relevant
+            chemical structures remains underexplored due to their
+            unavailability. Rare and unnatural sugars cannot be isolated from
+            nature and instead must be synthesized by laborious, specialized,
+            and often low-yielding multistep chemical or enzymatic syntheses. In
+            principle, biomass-derived monosaccharides (e.g., (D)-glucose,
+            (D)-mannose, (D)-galactose) provide an ideal starting material for
+            the synthesis of rare and unnatural sugars; however, carbohydrate
+            modification presents significant selectivity challenges. We
+            envision radical reactions can enable the divergent synthesis of
+            rare and unnatural sugars from unprotected biomass-derived
+            precursors (ie
+            <a
+              href="https://wendlandtlab.blob.core.windows.net/pubs/2020-Nature-Wang-article.pdf"
+              class="text-blue-700 font-bold"
+              >epimerization</a
+            >,
+            <a
+              href="https://wendlandtlab.blob.core.windows.net/pubs/2021-JACS-Carder-article.pdf"
+              class="text-blue-700 font-bold"
+              >deoxygenation</a
+            >).
+          </p>
+        </div>
+        <div class="mx-auto">
+          <img
+            src="../assets/research/sugars.svg?url"
+            alt="streptomycin structure"
+            class="
+              object-contain
+              overflow-hidden
+              rounded-md
+              py-2
+              w-screen-sm
+              lg:w-screen-lg
+              mx-auto
+            "
           />
         </div>
         <div class="text-xl font-sans font-bold my-5">Who funds us:</div>
