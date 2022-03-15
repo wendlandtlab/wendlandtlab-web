5029b2caf9b132a7efbdcc1ab79e4ea06cdfe63d
Gino Occhialini <gino.occhialini@gmail.com>
2022-03-15 19:11:42 -0400

    Add lab tour

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,43 +1,152 @@
 <template>
   <Popover class="relative bg-white">
     <div class="w-full mx-auto px-14 sm:px-6">
-      <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+      <div
+        class="
+          flex
+          justify-between
+          items-center
+          border-b-2 border-gray-100
+          py-6
+          md:justify-start md:space-x-10
+        "
+      >
         <div class="flex justify-start lg:w-0 lg:flex-1">
           <router-link to="/">
             <span class="sr-only">Workflow</span>
-            <img class="h-10 w-auto sm:h-12" src="../assets/wendlandtlablogo.svg?url" alt="" />
+            <img
+              class="h-10 w-auto sm:h-12"
+              src="../assets/wendlandtlablogo.svg?url"
+              alt=""
+            />
           </router-link>
         </div>
         <div class="-mr-2 -my-2 md:hidden">
-          <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <PopoverButton
+            class="
+              bg-white
+              rounded-md
+              p-2
+              inline-flex
+              items-center
+              justify-center
+              text-gray-400
+              hover:text-gray-500 hover:bg-gray-100
+              focus:outline-none
+              focus:ring-2
+              focus:ring-inset
+              focus:ring-indigo-500
+            "
+          >
             <span class="sr-only">Open menu</span>
             <MenuIcon class="h-6 w-6" aria-hidden="true" />
           </PopoverButton>
         </div>
         <PopoverGroup as="nav" class="hidden md:flex space-x-10">
-          <router-link to="/research" class="text-xl font-medium text-gray-500 hover:font-bold hover:text-gray-900">
+          <router-link
+            to="/research"
+            class="
+              text-xl
+              font-medium
+              text-gray-500
+              hover:font-bold hover:text-gray-900
+            "
+          >
             Research
           </router-link>
-          <router-link to="/people" class="text-xl font-medium text-gray-500 hover:font-bold hover:text-gray-900">
+          <router-link
+            to="/people"
+            class="
+              text-xl
+              font-medium
+              text-gray-500
+              hover:font-bold hover:text-gray-900
+            "
+          >
             People
           </router-link>
-          <router-link to="/publications" class="text-xl font-medium text-gray-500 hover:font-bold hover:text-gray-900">
+          <router-link
+            to="/publications"
+            class="
+              text-xl
+              font-medium
+              text-gray-500
+              hover:font-bold hover:text-gray-900
+            "
+          >
             Publications
+          </router-link>
+                    <router-link
+            to="/labtour"
+            class="
+              text-xl
+              font-medium
+              text-gray-500
+              hover:font-bold hover:text-gray-900
+            "
+          >
+            Lab space
           </router-link>
         </PopoverGroup>
       </div>
     </div>
 
-    <transition enter-active-class="duration-200 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-      <PopoverPanel focus class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-        <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+    <transition
+      enter-active-class="duration-200 ease-out"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="duration-100 ease-in"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <PopoverPanel
+        focus
+        class="
+          absolute
+          top-0
+          inset-x-0
+          p-2
+          transition
+          transform
+          origin-top-right
+          md:hidden
+        "
+      >
+        <div
+          class="
+            rounded-lg
+            shadow-lg
+            ring-1 ring-black ring-opacity-5
+            bg-white
+            divide-y-2 divide-gray-50
+          "
+        >
           <div class="pt-5 pb-6 px-5">
             <div class="flex items-center justify-between">
               <div>
-                <img class="h-10 w-auto" src="../assets/wendlandtlablogo.svg?url" alt="Wendlandt lab" />
+                <img
+                  class="h-10 w-auto"
+                  src="../assets/wendlandtlablogo.svg?url"
+                  alt="Wendlandt lab"
+                />
               </div>
               <div class="-mr-2">
-                <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <PopoverButton
+                  class="
+                    bg-white
+                    rounded-md
+                    p-2
+                    inline-flex
+                    items-center
+                    justify-center
+                    text-gray-400
+                    hover:text-gray-500 hover:bg-gray-100
+                    focus:outline-none
+                    focus:ring-2
+                    focus:ring-inset
+                    focus:ring-indigo-500
+                  "
+                >
                   <span class="sr-only">Close menu</span>
                   <XIcon class="h-6 w-6" aria-hidden="true" />
                 </PopoverButton>
@@ -46,14 +155,29 @@
           </div>
           <div class="py-6 px-5 space-y-6">
             <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-              <router-link to="/research" class="text-base font-bold text-gray-900 hover:text-gray-700">
+              <router-link
+                to="/research"
+                class="text-base font-bold text-gray-900 hover:text-gray-700"
+              >
                 Research
               </router-link>
-              <router-link to="/people" class="text-base font-bold text-gray-900 hover:text-gray-700">
+              <router-link
+                to="/people"
+                class="text-base font-bold text-gray-900 hover:text-gray-700"
+              >
                 People
               </router-link>
-              <router-link to="/publications" class="text-base font-bold text-gray-900 hover:text-gray-700">
+              <router-link
+                to="/publications"
+                class="text-base font-bold text-gray-900 hover:text-gray-700"
+              >
                 Publications
+              </router-link>
+                            <router-link
+                to="/labtour"
+                class="text-base font-bold text-gray-900 hover:text-gray-700"
+              >
+                Lab Space
               </router-link>
             </div>
           </div>
@@ -64,7 +188,12 @@
 </template>
 
 <script>
-import { Popover, PopoverButton, PopoverGroup, PopoverPanel } from '@headlessui/vue'
+import {
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/vue';
 import {
   BookmarkAltIcon,
   CalendarIcon,
@@ -78,8 +207,8 @@ import {
   SupportIcon,
   ViewGridIcon,
   XIcon,
-} from '@heroicons/vue/outline'
-import { ChevronDownIcon } from '@heroicons/vue/solid'
+} from '@heroicons/vue/outline';
+import { ChevronDownIcon } from '@heroicons/vue/solid';
 
 export default {
   components: {
@@ -92,8 +221,7 @@ export default {
     XIcon,
   },
   setup() {
-    return {
-    }
+    return {};
   },
-}
+};
 </script>
---
+++ src/pages/LabTour.vue
@@ -0,0 +1,111 @@
+<script lang="ts">
+</script>
+<template>
+  <div class="flex flex-col min-h-screen">
+    <HeaderMenu />
+    <div class="flex-grow mx-auto">
+      <section class="overflow-hidden text-gray-700">
+  <div class="container px-5 py-2 mx-auto lg:pt-10 lg:px-32">
+    <div class="flex flex-wrap -m-1 md:-m-2">
+      <div class="flex flex-wrap w-1/2">
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab1.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab2.png">
+        </div>
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab3.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab4.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab5.png">
+        </div>
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab6.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab7.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab8.png">
+        </div>
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab9.png">
+        </div>
+                <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab10.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab11.png">
+        </div>        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab12.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab13.png">
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/2">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab14.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab15.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab16.png">
+        </div>
+                <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab17.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab18.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab19.png">
+        </div>
+                <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab20.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab21.png">
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab22.png">
+        </div>
+                        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+            src="../assets/lab/lab23.png">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+    <FooterBox />
+  </div>
+</template>
