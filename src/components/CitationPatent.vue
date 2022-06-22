d35a087db25ad677a8783b417b84c1722a49e10e
Gino Occhialini <gino.occhialini@gmail.com>
2022-06-22 14:11:31 -0400

    Add patent citations

---
+++ src/components/CitationPatent.vue
@@ -0,0 +1,19 @@
+ <template>
+    <div class="mb-5">
+<a :href="Link">{{ Authors }} {{ Title }}. {{ Patent }}, {{ PubDate }}.</a>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Citation',
+  props: {
+    Authors: String,
+    Title: String,
+    Patent: String,
+    PubDate: String,
+    Link: String,
+  },
+});
+</script>
