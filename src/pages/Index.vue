b69e29bc50000489272441c6c934df627932c820
Gino Occhialini <gino.occhialini@gmail.com>
2023-09-11 22:01:11 -0400

    try to fix

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <HeaderMenu />
-    <div class="flex-grow bg-cover bg-no-repeat bg-top" :style='homepagebackstyle'></div>
+    <!-- <div class="flex-grow bg-cover bg-no-repeat bg-top" :style='homepagebackstyle'></div> -->
+    <div class="flex-grow bg-blue-50">    </div>
     <FooterBox />
   </div>
 </template>
